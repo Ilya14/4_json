5d55db83b4d73fd1b4f9d85b8a992f6bcd93a5ef
iBogdanov <ilya.bogdanov14@gmail.com>
2016-10-10 00:14:00 +0300

    Добавлен скрипт для вывода json в консоль в удобном формате

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,26 @@
+import sys
 import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
+
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent = 4, ensure_ascii = False))
+
 
 
 if __name__ == '__main__':
-    pass
+    if len (sys.argv) == 1:
+        filepath = input('Enter file path > ')
+    else:
+        if len (sys.argv) > 2:
+            print ("Error: too many parameters are transferred")
+            sys.exit (1)
+
+        filepath = sys.argv[1]
+    
+    pretty_print_json(load_data(filepath))
